389d1c3cec074f8749452b75409c7c14d0f896e0
mubasharakram786 <mubashar1418@gmail.com>
2025-11-06 18:22:30 +0500

    add and style hero component

--- app/routes/home/index.tsx
+++ app/routes/home/index.tsx
@@ -1,5 +1,5 @@
 import type { Route } from "./+types/index";
-
+import Hero from "~/components/Hero";
 export function meta({}: Route.MetaArgs) {
   return [
     { title: "Friendly Development" },
@@ -9,6 +9,6 @@ export function meta({}: Route.MetaArgs) {
 
 export default function Home() {
   return <div className="max-w-6xl mx-auto my-8">
-    My App
+    <Hero/>
   </div>;
 }
