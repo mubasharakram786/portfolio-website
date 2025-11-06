b29f082260fa94e2fa5c9a3448ac8261833b1a2e
mubasharakram786 <mubashar1418@gmail.com>
2025-11-06 18:01:43 +0500

    Feat: Mobile Nav

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,6 +1,8 @@
+import { useState } from "react"
 import { NavLink } from "react-router"
-import { FaLaptopCode } from "react-icons/fa"
+import { FaLaptopCode ,FaTimes , FaBars } from "react-icons/fa"
 const Navbar = () => {
+    const [isOpen ,setIsOpen] = useState(false)
     const base = 'transition hover:text-blue-400'
     const active = 'text-blue-400 font-semibold'
   return (
@@ -13,7 +15,11 @@ const Navbar = () => {
                 {/* Desktop Nav */}
                 <div className="hidden md:flex items-center gap-6">
                     <div className="space-x-4 text-sm text-gray-300">
-                        <NavLink className={({isActive})=> isActive ? active : base} to={'/'}>
+                        <NavLink 
+                        className={({isActive})=> isActive ? active : base}
+                         to={'/'}
+                        
+                        >
                             Home
                         </NavLink>
                         <NavLink className={({isActive})=> isActive ? active : base}  to={'/project'}>
@@ -30,8 +36,37 @@ const Navbar = () => {
                         </NavLink>
                     </div>
                 </div>
-            
+                <div className="md:hidden flex items-center gap-4">
+                    <button onClick={()=>setIsOpen(!isOpen)} className="text-blue-400 text-xl cursor-pointer" title="menu">
+                        {isOpen ? <FaTimes/> : <FaBars/>}                    
+                    </button>
+                </div>
         </div>
+        {/* Mobile Nav*/ }
+        {isOpen && (
+            <div className="bg-gray-800 border-t border-gray-700 px-6 py-4 space-y-2 space-x-4 text-center text-white md:hidden">
+                 <NavLink className={({isActive})=> isActive ? active : base} to={'/'}
+                     onClick={()=> setIsOpen(false)}>
+                            Home
+                        </NavLink>
+                        <NavLink className={({isActive})=> isActive ? active : base}  to={'/project'}
+                             onClick={()=> setIsOpen(false)}>
+                            Projects
+                        </NavLink>
+                        <NavLink className={({isActive})=> isActive ? active : base}  to={'/blog'}
+                             onClick={()=> setIsOpen(false)}>
+                            Blog
+                        </NavLink>
+                        <NavLink className={({isActive})=> isActive ? active : base}  to={'/about'}
+                             onClick={()=> setIsOpen(false)}>
+                            About
+                        </NavLink>
+                        <NavLink className={({isActive})=> isActive ? active : base}  to={'/contact'}
+                             onClick={()=> setIsOpen(false)}>
+                            Contact
+                        </NavLink>
+            </div>
+        )}
     </nav>
   )
 }
