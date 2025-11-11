1d6296a0be2bcf8abf9a1b1e9aab30d88aa865d7
mubasharakram786 <mubashar1418@gmail.com>
2025-11-11 18:18:39 +0500

    Add project type and project card component

--- app/routes/projects/index.tsx
+++ app/routes/projects/index.tsx
@@ -1,17 +1,23 @@
+import ProjectCard from "~/components/ProjectCard";
 import type { Route } from "./+types/index"
-
-export async function loader ({request}:Route.LoaderArgs):Promise<any>{
+import { Project } from "~/types";
+export async function loader ({request}:Route.LoaderArgs):Promise<{project:Project[]}>{
     const res = await fetch('http://localhost:8000/projects');
     const data = await res.json();
     return {projects:data}
 }
 const ProjectPage = ({loaderData}:Route.ComponentProps) => {
-    const {projects} = loaderData;
+    const {projects} = loaderData as {project:Project[]}; 
     console.log(projects)
   return (
     <>
 
       <div className='text-3xl text-white mb-8'>🚀 Projects</div>
+      <div className="grid gap-6 sm:grid-cols-2">
+        {projects.map((project)=>(
+          <ProjectCard project={project} key={project.id} />
+        ))}
+      </div>
     </>
   )
 }
